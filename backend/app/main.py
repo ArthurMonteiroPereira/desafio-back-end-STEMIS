5b050ba2c312ffe6cd5a4af4a65110a9ef05a416
ArthurMonteiroPereira2 <44785577+ArthurMonteiroPereira@users.noreply.github.com>
2025-05-13 18:11:33 -0300

    fix rota ia

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.api import usina, inversor, medicao
 from app.api import ingestao
 from app.api import agregacao
+from app.api import ia
 from app.api import processamento
 
 app = FastAPI()
@@ -11,6 +12,7 @@ app.include_router(inversor.router)
 app.include_router(medicao.router)
 app.include_router(ingestao.router)
 app.include_router(agregacao.router)
+app.include_router(ia.router)
 app.include_router(processamento.router)
 
 @app.get("/")
